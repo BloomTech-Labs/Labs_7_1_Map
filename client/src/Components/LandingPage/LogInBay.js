4c612ea6f3b20f686c2b366715c49f789176d82c
David Alexander Morales <damorales89@gmail.com>
2018-10-17 02:49:12 -0400

    Add: test modals for buttons

--- client/src/Components/LandingPage/LogInBay.js
+++ client/src/Components/LandingPage/LogInBay.js
@@ -1,17 +1,54 @@
 import React from 'react';
+import ReactModal from 'react-modal';
 
 import './LogInBay.css';
 
+ReactModal.setAppElement('div');
+
 class LogInBay extends React.Component {
-  state = {};
+  constructor() {
+    super();
+    this.state = {
+      showModal: false
+    };
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal() {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal() {
+    this.setState({ showModal: false });
+  }
 
   render() {
     return (
       <div className="LogInBay">
-        <button type="button" className="LogInBay-LogIn" onClick="">
+        <button
+          type="button"
+          className="LogInBay-LogIn"
+          onClick={this.handleOpenModal}
+        >
           Log In
         </button>
-        <button className="LogInBay-SignUp">Sign Up</button>
+        <button
+          type="button"
+          className="LogInBay-SignUp"
+          onClick={this.handleOpenModal}
+        >
+          Sign Up
+        </button>
+        <ReactModal
+          isOpen={this.state.showModal}
+          contentLabel="Example Text"
+          onRequestClose={this.handleCloseModal}
+          shouldCloseOnOverlayClick={true}
+        >
+          <p>Modal text!</p>
+          <button onClick={this.handleCloseModal}>Close Modal</button>
+        </ReactModal>
       </div>
     );
   }
