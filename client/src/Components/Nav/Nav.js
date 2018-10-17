24bd8227ada7dad5e3664ff104c922838f7f2028
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-10-17 15:07:25 -0700

    Convert Friends to dropdown using dummyData

--- client/src/Components/Nav/Nav.js
+++ client/src/Components/Nav/Nav.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './Nav.css';
 
+const friendsDummyData = [
+  'My Travels',
+  'Friend 1',
+  'Friend 2',
+  'Friend 3',
+  'Friend 4'
+];
+
 const Nav = () => {
   return (
     <div className="Nav">
       <div className="Nav__title">Scratch-N-Map</div>
 
       <div className="Nav__Center">
-        <div className="MenuItem Center__friends">Friends</div>
+        {/* <div className="MenuItem Center__friends">Friends</div> */}
+        <select name="My Travels" id="" className="MenuItem Center__friends">
+          {friendsDummyData.map((friend, i) => {
+            return i === 0 ? (
+              <option value="My Travels">My Travels</option>
+            ) : (
+              <option value={friend} key={i}>
+                {friend}
+              </option>
+            );
+          })}
+        </select>
         <input
           className="MenuItem Center__search"
           type="search"
