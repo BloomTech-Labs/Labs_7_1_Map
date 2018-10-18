08ccbede746ccb901690f140d0b4fe2327d2cff6
odax <nr10@albion.edu>
2018-10-18 13:36:23 -0400

    ran prettier on routes.js

--- server/api/routes.js
+++ server/api/routes.js
@@ -7,21 +7,21 @@ const authenticated = passport.authenticate('local', { session: false });
 const protect_middleware = passport.authenticate('jwt', { session: false });
 
 // export the routes
-module.exports = (server) => {
-	// general route
-	server.get('/', (req, res) => {
-		res.sendFile(path.join(__dirname + '/utils/landing.html'));
-	});
+module.exports = server => {
+  // general route
+  server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/utils/landing.html'));
+  });
 
-	server.get('/api', (req, res) => {
-		res.status(200).json({
-			msg: 'API is running....',
-		});
-	});
+  server.get('/api', (req, res) => {
+    res.status(200).json({
+      msg: 'API is running....'
+    });
+  });
 
-	server.get('/api/entry', protect_middleware, (req, res) => {
-		res.status(200).json({ msg: 'Entry allowed' });
-	});
-	server.route('/api/login').post(authenticated, login);
-	server.route('/api/register').post(create_user);
+  server.get('/api/entry', protect_middleware, (req, res) => {
+    res.status(200).json({ msg: 'Entry allowed' });
+  });
+  server.route('/api/login').post(authenticated, login);
+  server.route('/api/register').post(create_user);
 };
