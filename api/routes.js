f22587926af4991bed4149d924cacd6829075615
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-10-18 23:34:48 -0700

    Add change_email endpoint

--- api/routes.js
+++ api/routes.js
@@ -2,6 +2,7 @@ const {
   create_user,
   login,
   change_password,
+  change_email,
   facebook_login
 } = require('./controllers/user_controller');
 const passport = require('./utils/passport');
@@ -10,9 +11,9 @@ const path = require('path');
 // session is false so we can use jwt
 const authenticate = passport.authenticate('local', { session: false });
 const facebook_authintication = passport.authenticate('facebook', {
-	successRedirect: '/',
-	failureRedirect: '/login'
-})
+  successRedirect: '/',
+  failureRedirect: '/login'
+});
 const protected_route = passport.authenticate('jwt', { session: false });
 
 // export the routes
@@ -28,12 +29,13 @@ module.exports = server => {
     });
   });
 
-	server.get('/api/entry', protected_route, (req, res) => {
-		res.status(200).json({ msg: 'Entry allowed' });
-	});
+  server.get('/api/entry', protected_route, (req, res) => {
+    res.status(200).json({ msg: 'Entry allowed' });
+  });
 
-	server.route('/api/login').post(authenticate, login);
-	server.route('/api/facebook-login').post(facebook_login);
-	server.route('/api/register').post(create_user);
+  server.route('/api/login').post(authenticate, login);
+  server.route('/api/facebook-login').post(facebook_login);
+  server.route('/api/register').post(create_user);
   server.route('/api/change_password').post(protected_route, change_password);
+  server.route('/api/change_email').post(protected_route, change_email);
 };
