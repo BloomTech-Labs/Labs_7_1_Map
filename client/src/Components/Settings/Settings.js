25dfabf56f8e3f917e4026d38f433ca0f086f3fe
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-10-19 12:06:13 -0700

    Restructure Settings component into multiple forms

--- client/src/Components/Settings/Settings.js
+++ client/src/Components/Settings/Settings.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import axios from 'axios';
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 class Settings extends React.Component {
   state = {
+    currentPassword: '',
+    newEmail: '',
+    newPassword: '',
+    confirmNewPassword: '',
     autoScratch: false
   };
 
@@ -9,15 +16,44 @@ class Settings extends React.Component {
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
+    console.log(name, value);
 
     this.setState({
       [name]: value
     });
   };
 
-  handleSubmit = e => {
+  handleSubmitPassword = async e => {
     e.preventDefault();
-    console.log(e.target.new_email.value);
+    console.log(this.state);
+
+    const { currentPassword, newPassword, confirmNewPassword } = this.state;
+
+    // Error handling
+    if (!currentPassword) return console.error('Enter your current password!');
+    if (newPassword !== confirmNewPassword)
+      return console.error('Passwords do not match!');
+    if (newPassword.length < 6)
+      return console.error('Password needs to be at least 6 characters!');
+
+    const url = `${BACKEND_URL}/api/change_password`;
+    // TODO: pull username from global state and send in body
+    const body = { password: currentPassword, new_password: newPassword };
+    const options = {
+      headers: { Authorization: `Bearer ${localStorage.getItem('jwt_token')}` }
+    };
+
+    try {
+      const updatedUser = await axios.post(url, body, options);
+      console.log('Password updated successfully', updatedUser);
+    } catch (err) {
+      console.error('Error changing your password', err);
+    }
+  };
+
+  handleSubmitEmail = async e => {
+    e.preventDefault();
+    console.log(this.state.newEmail);
   };
 
   validateForm() {
@@ -28,60 +64,93 @@ class Settings extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor="newEmail">
-          Update Email
-          <input
-            type="text"
-            name="newEmail"
-            placeholder="New email"
-            onChange={e => this.handleChange(e)}
-          />
-        </label>
+      <div className="Settings">
+        {/* Change email form*/}
+        <form
+          onSubmit={this.handleSubmitEmail}
+          className="Settings__ChangePassword"
+        >
+          <label htmlFor="currentPassword">
+            Current Password
+            <input
+              type="text"
+              name="currentPassword"
+              placeholder="Current password"
+              onChange={e => this.handleChange(e)}
+            />
+          </label>
 
-        <label htmlFor="password">
-          Current Password
-          <input
-            type="text"
-            name="password"
-            placeholder="Current password"
-            onChange={e => this.handleChange(e)}
-          />
-        </label>
+          <label htmlFor="newEmail">
+            Update Email
+            <input
+              type="text"
+              name="newEmail"
+              placeholder="New email"
+              onChange={e => this.handleChange(e)}
+            />
+          </label>
 
-        <label htmlFor="newPassword">
-          New Password
-          <input
-            type="text"
-            name="newPassword"
-            placeholder="New password"
-            onChange={e => this.handleChange(e)}
-          />
-        </label>
+          <input type="submit" placeholder="Submit" />
+        </form>
 
-        <label htmlFor="confirmNewPassword">
-          Confirm New password
-          <input
-            type="text"
-            name="confirmNewPassword"
-            placeholder="Confirm new password"
-            onChange={e => this.handleChange(e)}
-          />
-        </label>
+        {/* Change password form*/}
+        <form
+          onSubmit={this.handleSubmitPassword}
+          className="Settings__ChangePassword"
+        >
+          <label htmlFor="currentPassword">
+            Current Password
+            <input
+              type="text"
+              name="currentPassword"
+              placeholder="Current password"
+              onChange={e => this.handleChange(e)}
+            />
+          </label>
 
-        <label htmlFor="autoScratch">
-          Auto Scratch Mode
-          <input
-            type="checkbox"
-            name="autoScratch"
-            checked={this.state.autoScratch}
-            onChange={e => this.handleChange(e)}
-          />
-        </label>
+          <label htmlFor="newPassword">
+            New Password
+            <input
+              type="text"
+              name="newPassword"
+              placeholder="New password"
+              onChange={e => this.handleChange(e)}
+            />
+          </label>
 
-        <br />
-        <input type="submit" placeholder="Submit" />
-      </form>
+          <label htmlFor="confirmNewPassword">
+            Confirm New password
+            <input
+              type="text"
+              name="confirmNewPassword"
+              placeholder="Confirm new password"
+              onChange={e => this.handleChange(e)}
+            />
+          </label>
+          <input type="submit" placeholder="Submit" />
+        </form>
+
+        {/* App preferences*/}
+        <div>
+          <label>
+            Theme
+            <select className="theme" name="Theme">
+              <option value="Light">Light</option>
+              <option value="Dark">Dark</option>
+            </select>
+          </label>
+
+          <label htmlFor="autoScratch">
+            Auto Scratch Mode
+            <input
+              type="checkbox"
+              name="autoScratch"
+              checked={this.state.autoScratch}
+              onChange={e => this.handleChange(e)}
+            />
+          </label>
+        </div>
+      </div>
     );
   }
 }
