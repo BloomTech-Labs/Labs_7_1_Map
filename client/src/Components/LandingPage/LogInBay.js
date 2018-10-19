a1f2d80954c88d14525115edd7492673237f777f
Edward Manda <lilkups@yahoo.com>
2018-10-19 17:58:10 -0500

    refactor landing page to automatic binding

--- client/src/Components/LandingPage/LogInBay.js
+++ client/src/Components/LandingPage/LogInBay.js
@@ -1,49 +1,54 @@
 import React from 'react';
 import ReactModal from 'react-modal';
 
+import { Link } from 'react-router-dom';
+
 import './LogInBay.css';
 
 ReactModal.setAppElement('div');
 
 class LogInBay extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			showModal: false,
-		};
-		this.handleOpenModal = this.handleOpenModal.bind(this);
-		this.handleCloseModal = this.handleCloseModal.bind(this);
-	}
+  state = {
+    showModal: false
+  };
 
-	handleOpenModal() {
-		this.setState({ showModal: true });
-	}
+  handleOpenModal = () => {
+    this.setState({ showModal: true });
+  };
 
-	handleCloseModal() {
-		this.setState({ showModal: false });
-	}
+  handleCloseModal = () => {
+    this.setState({ showModal: false });
+  };
 
-	render() {
-		return (
-			<div className="LogInBay">
-				<button type="button" className="LogInBay-LogIn" onClick={this.handleOpenModal}>
-					Log In
-				</button>
-				<button type="button" className="LogInBay-SignUp" onClick={this.handleOpenModal}>
-					Sign Up
-				</button>
-				<ReactModal
-					isOpen={this.state.showModal}
-					contentLabel="Example Text"
-					onRequestClose={this.handleCloseModal}
-					shouldCloseOnOverlayClick={true}
-				>
-					<p>Modal text!</p>
-					<button onClick={this.handleCloseModal}>Close Modal</button>
-				</ReactModal>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="LogInBay">
+        <button
+          type="button"
+          className="LogInBay-LogIn"
+          onClick={this.handleOpenModal}
+        >
+          Log In
+        </button>
+        <button
+          type="button"
+          className="LogInBay-SignUp"
+          onClick={this.handleOpenModal}
+        >
+          Sign Up
+        </button>
+        <ReactModal
+          isOpen={this.state.showModal}
+          contentLabel="Example Text"
+          onRequestClose={this.handleCloseModal}
+          shouldCloseOnOverlayClick={true}
+        >
+          <p>Modal text!</p>
+          <button onClick={this.handleCloseModal}>Close Modal</button>
+        </ReactModal>
+      </div>
+    );
+  }
 }
 
 export default LogInBay;
