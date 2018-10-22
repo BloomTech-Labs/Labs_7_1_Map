acd21cb1d4245b822fe76794fb016af79a6bbdf6
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-10-21 22:05:10 -0700

    Add basic Legend component

--- client/src/Components/Dashboard/Dashboard.js
+++ client/src/Components/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Map from '../Map/Map';
 import Nav from '../Nav/Nav';
+import Legend from '../Legend/Legend';
 import Settings from '../Settings/Settings';
 
 import './Dashboard.css';
@@ -18,6 +19,7 @@ class Dashboard extends Component {
     return (
       <div className="Dashboard">
         <Nav toggleSettings={this.toggleSettings} />
+        <Legend />
         <Map />
         {this.state.showingSettings && (
           <Settings onClick={this.toggleSettings} />
---
+++ client/src/Components/Legend/Legend.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Legend.css';
+
+const Legend = () => {
+  return (
+    <div className="Legend">
+      <ul>
+        <input type="checkbox" className="Legend__showFriends" /> Show Friends
+        <li>
+          <div className="Legend__swatch Visited" />
+          Visited
+        </li>
+        <li>
+          <div className="Legend__swatch LivedIn" />
+          Lived In
+        </li>
+        <li>
+          <div className="Legend__swatch Wishlist" />
+          Wishlist
+        </li>
+        <li>
+          <div className="Legend__swatch Home" />
+          Home
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Legend;
