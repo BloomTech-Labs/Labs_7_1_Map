5fb27c7e9faabbd0ec876067f3b054eb042efd0b
David Alexander Morales <damorales89@gmail.com>
2018-10-22 22:36:27 -0400

    Add: note get request route

--- api/notes/notesRoutes.js
+++ api/notes/notesRoutes.js
@@ -3,16 +3,16 @@ const router = express.Router();
 
 const Notes = require('./notesModel');
 
-router.route('/').get((req, res) => {
-  const query = Notes.find();
+router.route("/").get((req, res) => {
+	const query = Notes.find();
 
-  query
-    .then(notes => {
-      res.status(200).json(notes);
-    })
-    .catch(err => {
-      res.status(500).json({ errorMessage: 'Notes could not be retrieved' });
-    });
+	query
+		.then(notes => {
+			res.status(200).json(notes);
+		})
+		.catch(err => {
+			res.status(500).json({ errorMessage: "Notes could not be retrieved" });
+		});
 });
 
 module.exports = router;
