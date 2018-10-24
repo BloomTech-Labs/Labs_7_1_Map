65949c0a618be56852bf18bf85de51e3508ada5b
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-10-24 11:00:31 -0700

    Implement mongodb-memory-server for server.test.js

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,94 +1,107 @@
 const mongoose = require('mongoose');
+const MongodbMemoryServer = require('mongodb-memory-server').MongoMemoryServer;
 const request = require('supertest');
 const server = require('../server');
 const { make_token } = require('../api/utils/auth');
 
+const mongod = new MongodbMemoryServer();
 
 describe('User', () => {
-    const testuser = {
-        username: 'theTestAccount',
-        password: '123456',
-        email: 'email@email.com'
-    };
-    beforeAll(() => {
-        return mongoose
-          .connect('mongodb://localhost/test')
-          .then(() => console.log('\n==Connected to test database=='));
+  const testuser = {
+    username: 'theTestAccount',
+    password: '123456',
+    email: 'email@email.com'
+  };
+  // Connect to the mongodb before tests are run
+  beforeAll(async () => {
+    const uri = await mongod.getConnectionString();
+    await mongoose.connect(
+      uri,
+      { useNewUrlParser: true }
+    );
+  });
+
+  // Disconnect from mongodb after all tests are completed
+  afterAll(async () => {
+    await mongoose.disconnect();
+    mongod.stop();
+  });
+
+  //need to add tests to add checking for 0 character values
+
+  describe('post routes', () => {
+    describe('registration', () => {
+      it('detects invalid password', async () => {
+        const user = {
+          username: 'Pikachu',
+          password: 'p',
+          email: 'pika@gmail.com'
+        };
+        const response = await request(server)
+          .post('/api/register')
+          .send(user);
+        expect(response.status).toBe(400);
+      });
+      it('detects undefined password', async () => {
+        const user = { username: 'Pikachu', email: 'pika@gmail.com' };
+        const response = await request(server)
+          .post('/api/register')
+          .send(user);
+        expect(response.status).toBe(400);
+      });
+      it('detects undefined username', async () => {
+        const user = { password: 'pasdfsadfasdfsdf', email: 'pika@gmail.com' };
+        const response = await request(server)
+          .post('/api/register')
+          .send(user);
+        expect(response.status).toBe(400);
+      });
+      it('detects undefined username', async () => {
+        const user = { username: 'Pikachu', password: 'pasdfsadfasdfsdf' };
+        const response = await request(server)
+          .post('/api/register')
+          .send(user);
+        expect(response.status).toBe(400);
+      });
+      //need to work on test below, once deletion is implemented can test each one
+      it('rejects non-unique username/email', async () => {
+        const user = {
+          username: 'Pikachu',
+          password: 'pasdfsadfasdfsdf',
+          email: 'pika@gmail.com'
+        };
+        const response = await request(server)
+          .post('/api/register')
+          .send(user);
+        expect(response.status).toBe(500);
+      });
+      it('successfully creates new user (will fail after ran > 1, need delete request)', async () => {
+        const user = {
+          username: 'patrick',
+          password: 'pasdfsadfasdfsdf',
+          email: 'patrick@gmail.com'
+        };
+        const response = await request(server)
+          .post('/api/register')
+          .send(user);
+        expect(response.status).toBe(200);
+      });
     });
-
-    afterAll(() => {
-        return mongoose
-        .disconnect()
-        .then(() => console.log('\n==Disconnected from the test database=='));
+    describe('login', () => {
+      it('authorization success', async () => {
+        const user = { username: 'patrick', password: 'pasdfsadfasdfsdf' };
+        const response = await request(server)
+          .post('/api/login')
+          .send(user);
+        expect(response.status).toBe(200);
+      });
+      it('authorization fail', async () => {
+        const user = { username: 'patrick', password: 'pasdfsadfassdf' };
+        const response = await request(server)
+          .post('/api/login')
+          .send(user);
+        expect(response.status).toBe(401);
+      });
     });
-
-        it('10 === 10', () => {
-            expect(10).toBe(10);
-        })
-
-    //need to add tests to add checking for 0 character values
-
-    describe('post routes', () => {
-        describe('registration', () => {
-            it('detects invalid password', async() => {
-                const user = {username: 'Pikachu', password: 'p', email: 'pika@gmail.com'};
-                const response = await request(server)
-                .post('/api/register')
-                .send(user);
-                expect(response.status).toBe(400);
-            })
-            it('detects undefined password', async() => {
-                const user = {username: 'Pikachu', email: 'pika@gmail.com'};
-                const response = await request(server)
-                .post('/api/register')
-                .send(user);
-                expect(response.status).toBe(400);
-            })
-            it('detects undefined username', async() => {
-                const user = {password: 'pasdfsadfasdfsdf', email: 'pika@gmail.com'};
-                const response = await request(server)
-                .post('/api/register')
-                .send(user);
-                expect(response.status).toBe(400);
-            })
-            it('detects undefined username', async() => {
-                const user = {username: 'Pikachu', password: 'pasdfsadfasdfsdf'};
-                const response = await request(server)
-                .post('/api/register')
-                .send(user);
-                expect(response.status).toBe(400);
-            })
-            //need to work on test below, once deletion is implemented can test each one
-            it('rejects non-unique username/email', async() => {
-                const user = {username: 'Pikachu', password: 'pasdfsadfasdfsdf', email: 'pika@gmail.com'};
-                const response = await request(server)
-                .post('/api/register')
-                .send(user);
-                expect(response.status).toBe(500);
-            })
-            it('successfully creates new user (will fail after ran > 1, need delete request)', async() => {
-                const user = {username: 'patrick', password: 'pasdfsadfasdfsdf', email: 'patrick@gmail.com'};
-                const response = await request(server)
-                .post('/api/register')
-                .send(user);
-                expect(response.status).toBe(200);
-            })
-        })
-        describe('login', () => {
-            it('authorization success', async() => {
-                const user = {username: 'patrick', password: 'pasdfsadfasdfsdf'};
-                const response = await request(server)
-                .post('/api/login')
-                .send(user);
-                expect(response.status).toBe(200);
-            })
-            it('authorization fail', async() => {
-                const user = {username: 'patrick', password: 'pasdfsadfassdf'};
-                const response = await request(server)
-                .post('/api/login')
-                .send(user);
-                expect(response.status).toBe(401);
-            })
-        })
-    })
+  });
 });
