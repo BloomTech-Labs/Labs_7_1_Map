6b4f83d4bb6e49709b28b8086da6b6f51baf0352
David Alexander Morales <damorales89@gmail.com>
2018-10-24 00:26:52 -0400

    stuck on async/await

--- api/models/notesModel.js
+++ api/models/notesModel.js
@@ -17,11 +17,11 @@ const NoteSchema = new Schema(
 //note = new Note({text: 'some note', user: <the user id for user trying to save note>})
 //note.saveNote()
 
-// NoteSchema.methods.saveNote = async () => {
-//   await this.save();
-//   const user = await User.find({ _id: this.user });
-//   user.notes.push(this._id);
-//   await user.save();
-// };
+NoteSchema.methods.saveNote = async () => {
+  await this.save();
+  const user = await User.find({ _id: this.user });
+  user.notes.push(this._id);
+  await user.save();
+};
 
 module.exports = mongoose.model('Note', NoteSchema);
