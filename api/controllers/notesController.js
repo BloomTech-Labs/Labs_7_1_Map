452bdc9f00f55a6675e18b1980c8a7130c364ab9
David Alexander Morales <damorales89@gmail.com>
2018-10-25 02:37:25 -0400

    Change: Continuing to refactor after writing incorrect syntax

--- api/controllers/notesController.js
+++ api/controllers/notesController.js
@@ -1,54 +1,44 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 const notesModel = require('./models/notesModel');
 
-//Middleware to Handle Errors
-const awaitErrorHandlerFactory = middleware => {
-  return async (req, res, next) => {
-    try {
-      await middleware(req, res, next);
-    } catch (err) {
-      next(err);
-    }
-  };
-};
+// //Middleware to Handle Errors
+// const awaitErrorHandlerFactory = middleware => {
+//   return async (req, res, next) => {
+//     try {
+//       await middleware(req, res, next);
+//     } catch (err) {
+//       next(err);
+//     }
+//   };
+// };
 
-// Get Notes
-router.get(
-  '/api/notes',
+module.exports = {
+  // Get Notes
+  getNoteById: ('/api/notes',
   awaitErrorHandlerFactory(async (req, res, next) => {
     const note = await notesModels.NoteSchema.findAll({});
     return res.json({
       error: false,
       data: note
     });
-  })
-);
+  })),
 
-// Post Note
-router.post(
-  '/api/notes',
-  awaitErrorHandlerFactory(async (req, res, next) => {
+  // Post Note
+  postNote: async (req, res, next) => {
     const { text } = req.body;
     const note = notesModel.NoteSchema.save({
-      text: text,
-      user: {
-        type: ObjectIdSchema,
-        ref: 'User'
-      },
-      timestamps: timestamps
+      text: text
     });
     return res.status(201).json({
       error: false,
       data: note,
       message: 'Country note has been created.'
     });
-  })
-);
+  },
 
-// Update Note
-router.put(
-  '/api/note/:id',
+  // Update Note
+  updateNote: ('/api/note/:id',
   awaitErrorHandlerFactory(async (req, res, next) => {
     const noteId = req.params.id;
     const { text } = req.body;
@@ -58,12 +48,10 @@ router.put(
       error: false,
       message: 'Country note has been updated.'
     });
-  })
-);
+  })),
 
-// Delete Note
-router.delete(
-  '/api/note/:id', 
+  // Delete Note
+  deleteNote: ('/api/note/:id',
   awaitErrorHandlerFactory(async (req, res, next) => {
     const noteId = req.params.id;
 
@@ -74,8 +62,7 @@ router.delete(
     });
     return res.status(201).json({
       error: false,
-      message: "Country note has been deleted."
+      message: 'Country note has been deleted.'
     });
-  })
-);
-module.exports = router;
+  }))
+};
