a8f773177f2dc348b18f9f6ad4e1950671dfa3d3
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-10-24 17:45:49 -0700

    Add tests for get_user route

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -40,7 +40,7 @@ describe('User', () => {
 
   //need to add tests to add checking for 0 character values
 
-  describe('post routes', () => {
+  describe('POST routes', () => {
     describe('registration', () => {
       it('detects invalid password', async () => {
         const user = {
@@ -154,7 +154,6 @@ describe('User', () => {
           .post('/api/login')
           .send(user);
 
-        console.log(response.body)
         expect(response.status).toBe(200);
         expect(response.body.user).toBeDefined();
         expect(response.body.user._id).toBeDefined();
@@ -173,6 +172,73 @@ describe('User', () => {
     });
   });
   describe('GET routes', () => {
+    it('get_user retrieves a user with a valid token', async () => {
+      const testUserInfo = {
+        username: 'getuser1',
+        password: '123456',
+        email: 'getuser1@test.com'
+      };
+      const newUser = await request(server)
+        .post('/api/register')
+        .send(testUserInfo);
+      const { jwt_token, user } = newUser.body;
 
-  })
+      console.log(user)
+      const getUser = await request(server)
+        .get(`/api/get_user/${user.id}`)
+        .set('Authorization', `Bearer ${jwt_token}`);
+
+
+      expect(newUser).toBeDefined();
+      expect(jwt_token).toBeDefined();
+      expect(getUser.body._id).toBe(user.id);
+      expect(getUser.body.username).toBe('getuser1');
+      expect(getUser.body.email).toBe('getuser1@test.com');
+    });
+
+    it('get_user fails without a token', async () => {
+      const testUserInfo = {
+        username: 'getuser2',
+        password: '123456',
+        email: 'getuser2@test.com'
+      };
+      const newUser = await request(server)
+        .post('/api/register')
+        .send(testUserInfo);
+      const { jwt_token, user } = newUser.body;
+
+      const getUser = await request(server)
+        .get(`/api/get_user/${user._id}`)
+
+      expect(newUser).toBeDefined();
+      expect(jwt_token).toBeDefined();
+      expect(getUser.status).toBe(401);
+      expect(getUser.body._id).toBeUndefined();
+      expect(getUser.body.username).toBeUndefined();
+      expect(getUser.body.email).toBeUndefined();
+    });
+
+    it('get_user fails with an invalid token', async () => {
+      const testUserInfo = {
+        username: 'getuser3',
+        password: '123456',
+        email: 'getuser3@test.com'
+      };
+      const newUser = await request(server)
+        .post('/api/register')
+        .send(testUserInfo);
+      const { jwt_token, user } = newUser.body;
+
+      const getUser = await request(server)
+        .get(`/api/get_user/${user._id}`)
+        .set('Authorization', `Bearer ${jwt_token.slice(1)}`);
+
+      expect(newUser).toBeDefined();
+      expect(jwt_token).toBeDefined();
+      expect(getUser.status).toBe(401);
+      expect(getUser.body._id).toBeUndefined();
+      expect(getUser.body.username).toBeUndefined();
+      expect(getUser.body.email).toBeUndefined();
+    });
+  });
 });
