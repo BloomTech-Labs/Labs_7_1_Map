a31d2d2feac2d236ee9239ef8eed8353ece7af73
Edward Manda <lilkups@yahoo.com>
2018-10-25 15:29:53 -0500

    removed the notes field from user model

--- api/models/user.js
+++ api/models/user.js
@@ -43,12 +43,6 @@ const UserSchema = new Schema(
         provider: String,
         id: String
       }
-    ],
-    notes: [
-      {
-        type: ObjectIdSchema,
-        ref: 'Note'
-      }
     ]
   },
   {
