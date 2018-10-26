86e836e8e10ac3e40755ba4929182db517836e2c
David Alexander Morales <damorales89@gmail.com>
2018-10-26 05:28:16 -0400

    Add: Got a working slider, just need to add the pieces to it

--- client/src/Components/BorderBay/BorderBay.js
+++ client/src/Components/BorderBay/BorderBay.js
@@ -1,62 +1,64 @@
 import React, { Component } from 'react';
-import Slider from 'rc-slider/lib/Slider';
+import Nouislider from 'nouislider-react';
 
 import './BorderBay.css';
-import 'rc-slider/assets/index.css';
+import 'nouislider/distribute/nouislider.css';
 
-const marks = {
-  0: 'No Interest',
-  1: {
-    style: {
-      color: 'pink'
-    },
-    label: 'Wishlist'
-  },
-  2: {
-    style: {
-      color: 'yellow'
-    },
-    label: 'Transited'
-  },
-  3: {
-    style: {
-      color: 'green'
-    },
-    label: 'Visited'
-  },
-  4: {
-    style: {
-      color: 'blue'
-    },
-    label: 'Lived'
-  }
-};
+// import Slider from 'rc-slider/lib/Slider';
+// import 'rc-slider/assets/index.css';
+
+// const marks = {
+//   0: 'No Interest',
+//   1: {
+//     style: {
+//       color: 'pink'
+//     },
+//     label: 'Wishlist'
+//   },
+//   2: {
+//     style: {
+//       color: 'yellow'
+//     },
+//     label: 'Transited'
+//   },
+//   3: {
+//     style: {
+//       color: 'green'
+//     },
+//     label: 'Visited'
+//   },
+//   4: {
+//     style: {
+//       color: 'blue'
+//     },
+//     label: 'Lived'
+//   }
+// };
+
+// function log(value) {
+//   console.log(value); //eslint-disable-line
+// }
 
 export default class BorderBay extends Component {
-  handleSlider(marks) {
-    this.setState({ marks });
-  }
-
-  log(marks) {
-    console.log(marks); //eslint-disable-line
-  }
-
   render() {
     return (
       <div className="Country_Border">
         <p className="Country_Border-Border">Border</p>
         <div className="Country_Border-Slider">
           <p className="Slide-Tag">Level of Stay</p>
-          <Slider
+          <Nouislider range={{ min: 0, max: 100 }} start={[20, 80]} connect />
+
+
+          {/* <Slider
             className="Slider1"
             min={0}
             max={4}
             marks={marks}
             step={null}
-            onChange={this.log}
+            onChange={log}
             defaultValue={0}
             // value={0}
-          />
+          /> */}
         </div>
       </div>
     );
