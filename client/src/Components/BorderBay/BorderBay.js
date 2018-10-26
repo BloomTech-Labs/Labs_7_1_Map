7be75a1ab7a3d47bc6deeea1a68cbca0207e2f0a
David Alexander Morales <damorales89@gmail.com>
2018-10-26 15:46:32 -0400

    Change: Slider values reflect status

--- client/src/Components/BorderBay/BorderBay.js
+++ client/src/Components/BorderBay/BorderBay.js
@@ -4,13 +4,15 @@ import Nouislider from 'nouislider-react';
 import './BorderBay.css';
 import 'nouislider/distribute/nouislider.css';
 
-const stayValue = ['Uninterested', 'Wishlist', 'Transited', 'Visited', 'Lived'];
+const StayValue = {
+  0: 'Uninterested',
+  1: 'Wishlist',
+  2: 'Transited',
+  3: 'Visited',
+  4: 'Lived'
+};
 
 export default class BorderBay extends Component {
-  state = {
-    values: ''
-  };
-
   render() {
     return (
       <div className="Country_Border">
@@ -18,25 +20,26 @@ export default class BorderBay extends Component {
         <div className="Country_Border-Slider">
           <p className="Slider-Tag">Level of Stay</p>
           <Nouislider
-            start={[0]}
-            pips={{ mode: 'count', values: 5 }}
-            clickablePips
-            range={{
-              min: 0,
-              max: 5
-            }}
-          />
-
-          {/* <Nouislider
-            key={item}
-            id={item}
+            accessibility
             start={0}
             step={1}
-            range={{ min: 0, max: 4 }}
-            pip={{ mode: 'count', values: 5 }}
-            clickablePips={true}
-            tooltip={[{ 0: 'Wishlist', 2: 'Visited' }]}
-          /> */}
+            pips={{
+              mode: 'range',
+              format: {
+                to: function(change) {
+                  return StayValue[change];
+                }
+              }
+            }}
+            clickablePips
+            range={{
+              min: [0, 1],
+              '25%': [1, 2],
+              '50': [2, 3],
+              '75': [3, 2],
+              max: [4, 3]
+            }}
+          />
         </div>
       </div>
     );
