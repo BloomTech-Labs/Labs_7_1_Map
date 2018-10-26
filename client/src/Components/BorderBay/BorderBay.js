641d6b5f23bd52232edab9d89589cb9e644119c2
David Alexander Morales <damorales89@gmail.com>
2018-10-26 06:20:21 -0400

    Change: cleaned up BorderBay, slider moves just needs more customization and still to be linked to border and colors

--- client/src/Components/BorderBay/BorderBay.js
+++ client/src/Components/BorderBay/BorderBay.js
@@ -4,40 +4,13 @@ import Nouislider from 'nouislider-react';
 import './BorderBay.css';
 import 'nouislider/distribute/nouislider.css';
 
-// import Slider from 'rc-slider/lib/Slider';
-// import 'rc-slider/assets/index.css';
-
-// const marks = {
-//   0: 'No Interest',
-//   1: {
-//     style: {
-//       color: 'pink'
-//     },
-//     label: 'Wishlist'
-//   },
-//   2: {
-//     style: {
-//       color: 'yellow'
-//     },
-//     label: 'Transited'
-//   },
-//   3: {
-//     style: {
-//       color: 'green'
-//     },
-//     label: 'Visited'
-//   },
-//   4: {
-//     style: {
-//       color: 'blue'
-//     },
-//     label: 'Lived'
-//   }
-// };
-
-// function log(value) {
-//   console.log(value); //eslint-disable-line
-// }
+const marks = {
+  0: 'Uninterested',
+  1: 'Wishlist',
+  2: 'Transited',
+  3: 'Visited',
+  4: 'Lived'
+};
 
 export default class BorderBay extends Component {
   render() {
@@ -46,19 +19,15 @@ export default class BorderBay extends Component {
         <p className="Country_Border-Border">Border</p>
         <div className="Country_Border-Slider">
           <p className="Slide-Tag">Level of Stay</p>
-          <Nouislider range={{ min: 0, max: 100 }} start={[20, 80]} connect />
-
-
-          {/* <Slider
-            className="Slider1"
-            min={0}
-            max={4}
-            marks={marks}
-            step={null}
-            onChange={log}
-            defaultValue={0}
-            // value={0}
-          /> */}
+          <Nouislider
+            start={0}
+            step={1}
+            range={{ min: 0, max: 4 }}
+            pip={{ mode: 'count', values: marks }}
+            clickablePips={true}
+            connect={true}
+            tooltip={[{ 0: 'Wishlist', 2: 'Visited' }]}
+          />
         </div>
       </div>
     );
