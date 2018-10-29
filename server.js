e91cfe8c6ea08c2fb22d517b139c2825a696452e
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-10-29 15:18:09 -0700

    Add import for ; improve comments

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const path = require('path');
 
 // bring in the routes
 const routes = require('./api/routes');
@@ -15,7 +16,7 @@ server.use(express.json());
 server.use(morgan('dev'));
 server.use(helmet());
 
-//cross origin request sharing permissions
+// cross origin request sharing permissions
 const corsOptions = {
   origin: '*',
   credentials: true
@@ -23,7 +24,9 @@ const corsOptions = {
 
 server.use(cors(corsOptions));
 
-// Serve up static client files (in `/client/build` folder) at root endpoint
+// Serve up static client files (in `/client/build` folder) at root endpoint.
+//    Static files are created by the `heroku-postbuild` script in package.json
+//    during deployment
 server.use(express.static(path.join(__dirname, 'client/build')));
 
 // pass the server to the routes
