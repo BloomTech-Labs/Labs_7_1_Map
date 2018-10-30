32e31a45a526df3af773d6928378a7098f601f40
odax <nr10@albion.edu>
2018-10-30 12:46:44 -0400

    query replace works, find does not yet

--- api/controllers/statusController.js
+++ api/controllers/statusController.js
@@ -3,10 +3,17 @@ const user = require ('../models/user');
 module.exports = {
   handle_status: async (req, res) => {
     try {
-      res.status(200).json(req.body);
-      console.log(req.body);
+      user.find({countries: {$elemMatch: {country_code:'CHN'}}})
+        .then(result => {
+          console.log(result);
+        })
+      // user.updateOne(
+      //   { "countries.country_code": "CHN"},
+      //   { "$set": { "countries.$.country_code": "USA"}}
+      // )
+      res.status(200).json(result);
     } catch (err) {
-      res.status(399).json({message: 'there was an error!'})
+      res.status(399).json({message: 'there was an error!', err})
     }
   }
 };
