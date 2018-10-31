06613e2133284b43411ac3d86d24cad6784e3f01
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-10-31 12:45:57 -0700

    Add defaultValue to theme dropdown that uses user's preferences

--- client/src/Components/Settings/Preferences.js
+++ client/src/Components/Settings/Preferences.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import mapTilesUrls from '../Map/mapSetup.js';
 
 import './Preferences.css';
 
@@ -18,14 +19,23 @@ class Preferences extends Component {
   };
 
   render() {
+    const theme =
+      this.props.user && this.props.user.preferences
+        ? this.props.user.preferences.theme
+        : 'dark';
     return (
       <div className="Settings__Preferences">
         <h4>Preferences</h4>
         <div className="Preferences__theme">
           <h5>Theme</h5>
-          <select className="Theme" name="theme" onChange={this.handleChange}>
-            <option value="light">Light</option>
-            <option value="dark">Dark</option>
+          <select
+            className="Theme"
+            defaultValue={theme}
+            name="theme"
+            onChange={this.handleChange}
+          >
+            <option value="light">light</option>
+            <option value="dark">dark</option>
           </select>
         </div>
 
