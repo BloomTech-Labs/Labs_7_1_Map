c1516ad9b57cdd9e942451a0ae0474869ca8232e
Edward Manda <lilkups@yahoo.com>
2018-11-03 17:16:48 -0500

    added signup component

---
+++ client/src/Components/LandingPage/Signup.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import './Signup.css';
+
+const Signup = props => {
+  return (
+    <div className="Signup">
+      <form className="Signup__SignupForm" onSubmit={props.handleSignUp}>
+        <div className="SignupForm__title">Sign Up</div>
+        <input
+          type="text"
+          onChange={props.handleChange}
+          value={props.username}
+          placeholder="Username"
+          name="username"
+        />
+        <input
+          type="email"
+          onChange={props.handleChange}
+          value={props.email}
+          placeholder="email address"
+          name="email"
+        />
+        <input
+          type="password"
+          onChange={props.handleChange}
+          value={props.password}
+          placeholder="Password"
+          name="password"
+        />
+        <input
+          type="password"
+          onChange={props.handleChange}
+          value={props.passwordComfirm}
+          placeholder="Password again"
+          name="passwordComfirm"
+        />
+        <button className="SignupForm__Button">Sign Up</button>
+        <div className="danger">{props.error ? props.error : ''}</div>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
