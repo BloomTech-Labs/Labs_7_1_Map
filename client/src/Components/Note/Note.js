bc8e1ca668e9f2c4122efcb9712c61e370aa0276
Edward Manda <lilkups@yahoo.com>
2018-11-03 13:31:25 -0500

    refactored the notes component from class to functional

--- client/src/Components/Note/Note.js
+++ client/src/Components/Note/Note.js
@@ -1,52 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import './Note.css';
 
-class Note extends Component {
-  render() {
-    let display;
-    if (this.props.country.editNoteMode) {
+const Note = props => {
+  let display;
+  if (props.country.editNoteMode) {
+    display = (
+      <div className="Notes__EditMode">
+        <FontAwesomeIcon
+          className="EditMode__save"
+          onClick={props.handleUpdateNotes}
+          icon="save"
+        />
+        <textarea
+          name="notes"
+          onChange={props.handleChangeNote}
+          value={props.country.notes}
+        />
+      </div>
+    );
+  } else {
+    if (props.country.notes) {
       display = (
-        <div className="Notes__EditMode">
+        <div className="Notes__ViewMode">
           <FontAwesomeIcon
-            className="EditMode__save"
-            onClick={this.props.handleUpdateNotes}
-            icon="save"
+            className="ViewMode__edit"
+            onClick={props.turnOnEditNote}
+            icon="edit"
           />
-          <textarea
-            name="notes"
-            onChange={this.props.handleChangeNote}
-            value={this.props.country.notes}
-          />
+          <div className="ViewMode__Notes">{props.country.notes}</div>
         </div>
       );
     } else {
-      if (this.props.country.notes) {
-        display = (
-          <div className="Notes__ViewMode">
-            <FontAwesomeIcon
-              className="ViewMode__edit"
-              onClick={this.props.turnOnEditNote}
-              icon="edit"
-            />
-            <div className="ViewMode__Notes">{this.props.country.notes}</div>
-          </div>
-        );
-      } else {
-        display = (
-          <div className="Notes__ViewMode">
-            <FontAwesomeIcon
-              className="ViewMode__add"
-              onClick={this.props.turnOnEditNote}
-              icon="plus-circle"
-            />
-            <span>Add a note</span>
-          </div>
-        );
-      }
+      display = (
+        <div className="Notes__ViewMode">
+          <FontAwesomeIcon
+            className="ViewMode__add"
+            onClick={props.turnOnEditNote}
+            icon="plus-circle"
+          />
+          <span>Add a note</span>
+        </div>
+      );
     }
-    return <div className="Note">{display}</div>;
   }
-}
+  return <div className="Note">{display}</div>;
+};
 export default Note;
