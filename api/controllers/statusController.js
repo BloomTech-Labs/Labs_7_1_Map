60a2c24696c16b79719d069e51906a513fa7b17e
Edward Manda <lilkups@yahoo.com>
2018-11-02 23:40:34 -0500

    changed the handle_status to async

--- api/controllers/statusController.js
+++ api/controllers/statusController.js
@@ -1,94 +1,135 @@
 const User = require('../models/user');
-
-//note from nalee
-//current route for this is localhost:8000/api/country_status
-//it currently is accepting a json object with the following structure
-/*
-{
-  country_code: '',
-  status_code: '',
-  name: '' (name of country)
-  username: '',
-}
-*/
+const DEV = process.env.DEV || true;
 
 module.exports = {
-  handle_status: (req, res) => {
-    const { country_code, status_code, username, name, scratched } = req.body;
+  update_notes: async (req, res) => {
+    try {
+    } catch (err) {
+      if (DEV) console.log(err);
+      return res.status(500).json({ error: 'Internal server error!' });
+    }
+  },
 
-    //below are queries
-    const queryUserCountry = {
-      $and: [
-        { username: username },
-        {
+  handle_scratched: async (req, res) => {
+    try {
+    } catch (err) {
+      if (DEV) console.log(err);
+      return res.status(500).json({ error: 'Internal server error!' });
+    }
+  },
+
+  handle_status: async (req, res) => {
+    try {
+      const { country_code, status_code, username, name, scratched } = req.body;
+
+      //below are queries
+      const queryUserCountry = {
+        $and: [
+          { username: username },
+          {
+            countries: {
+              $elemMatch: { country_code: country_code }
+            }
+          }
+        ]
+      };
+      const queryUser = {
+        username: username
+      };
+
+      //below are operators
+      const editCountry = {
+        $set: {
+          'countries.$.status_code': status_code,
+          'countries.$.scratched': scratched
+        }
+      };
+
+      const createCountry = {
+        $push: {
           countries: {
-            $elemMatch: { country_code: country_code }
+            country_code: country_code,
+            name: name,
+            status_code: status_code,
+            notes: '',
+            scratched: true
           }
         }
-      ]
-    };
-    const queryUser = {
-      username: username
-    };
+      };
 
-    //below are operators
-    const editCountry = {
-      $set: {
-        'countries.$.status_code': status_code,
-        'countries.$.scratched': scratched
-      }
-    };
+      //below is the option to return the document, after it is edited, from
+      //findOneAndUpdate()
 
-    const createCountry = {
-      $push: {
-        countries: {
-          country_code: country_code,
-          name: name,
-          status_code: status_code,
-          notes: ''
-          // TODO: add scratched here whenever slider is disabled
-        }
-      }
-    };
+      const options = {
+        //cant get this to work
+        new: true
+        // {returnOriginal:false}
+      };
 
-    //below is the option to return the document, after it is edited, from
-    //findOneAndUpdate()
+      //logic for below:
+      //1. searches for document that includes user & country (findOne)
+      //2. If fails, that means user does not have the country
+      // it will create a new country object for that use with all of the information from the req.body (updateOne)
+      //3. If #1 succeeds, it will update the user countries code with the status_code from the req.body (findOneAndUpdate)
+      const found_user_country = await User.findOne(queryUserCountry);
 
-    const options = {
-      //cant get this to work
-      new: true
-      // {returnOriginal:false}
-    };
-
-    //logic for below:
-    //1. searches for document that includes user & country (findOne)
-    //2. If fails, that means user does not have the country
-    // it will create a new country object for that use with all of the information from the req.body (updateOne)
-    //3. If #1 succeeds, it will update the user countries code with the status_code from the req.body (findOneAndUpdate)
-    User.findOne(queryUserCountry)
-      .then(result => {
-        if (result !== null) {
-          User.findOneAndUpdate(queryUserCountry, editCountry, options)
-            .then(newDoc => {
-              res.status(200).json(newDoc);
-            })
-            .catch(err => {
-              res
-                .status(400)
-                .json({ error: 'failure to update country status' });
-            });
+      if (found_user_country) {
+        // update the country in user
+        const updated_country = await User.findOneAndUpdate(
+          queryUserCountry,
+          editCountry,
+          options
+        );
+        if (updated_country) {
+          res.status(200).json(updated_country);
         } else {
-          User.findOneAndUpdate(queryUser, createCountry, options)
-            .then(newDoc => {
-              res.status(201).json(newDoc);
-            })
-            .catch(err => {
-              res.status(409).json({ error: 'failure to create new country' });
-            });
+          res.status(400).json({ error: 'failure to update country status' });
         }
-      })
-      .catch(err => {
-        res.status(500).json({ error: 'failure to initiate query' });
-      });
+      } else {
+        // create new country by updating the user
+        const found_user = await User.findOneAndUpdate(
+          queryUser,
+          createCountry,
+          options
+        );
+        if (found_user) {
+          res.status(200).json(found_user);
+        } else {
+          res.status(409).json({ error: 'failure to create new country' });
+        }
+      }
+
+      /*const 
+      
+      User.findOne(queryUserCountry)
+        .then(result => {
+          if (result !== null) {
+            User.findOneAndUpdate(queryUserCountry, editCountry, options)
+              .then(newDoc => {
+                res.status(200).json(newDoc);
+              })
+              .catch(err => {
+                res
+                  .status(400)
+                  .json({ error: 'failure to update country status' });
+              });
+          } else {
+            User.findOneAndUpdate(queryUser, createCountry, options)
+              .then(newDoc => {
+                res.status(201).json(newDoc);
+              })
+              .catch(err => {
+                res.status(409).json({ error: 'failure to create new country' });
+              });
+          }
+        })
+        .catch(err => {
+          res.status(500).json({ error: 'failure to initiate query' });
+        });
+        */
+    } catch (err) {
+      if (DEV) console.log(err);
+      return res.status(500).json({ error: 'Internal server error!' });
+    }
   }
 };
