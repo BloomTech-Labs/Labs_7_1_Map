3eea11d55aac3dee302fc6d9647d004607208b4f
Edward Manda <lilkups@yahoo.com>
2018-11-03 01:16:09 -0500

    created an update end point for notes

--- api/controllers/statusController.js
+++ api/controllers/statusController.js
@@ -2,14 +2,6 @@ const User = require('../models/user');
 const DEV = process.env.DEV || true;
 
 module.exports = {
-  update_notes: async (req, res) => {
-    try {
-    } catch (err) {
-      if (DEV) console.log(err);
-      return res.status(500).json({ error: 'Internal server error!' });
-    }
-  },
-
   handle_scratched: async (req, res) => {
     try {
       const { country_code, username, name, scratched, notes } = req.body;
@@ -156,6 +148,67 @@ module.exports = {
           res.status(200).json(updated_country);
         } else {
           res.status(400).json({ error: 'failure to update country status' });
+        }
+      } else {
+        // create new country by updating the user
+        const found_user = await User.findOneAndUpdate(
+          queryUser,
+          createCountry,
+          options
+        );
+        if (found_user) {
+          res.status(200).json(found_user);
+        } else {
+          res.status(409).json({ error: 'failure to create new country' });
+        }
+      }
+    } catch (err) {
+      if (DEV) console.log(err);
+      return res.status(500).json({ error: 'Internal server error!' });
+    }
+  },
+
+  update_notes: async (req, res) => {
+    try {
+      const { country_code, username, name, notes } = req.body;
+      //below are queries
+      const queryUserCountry = {
+        $and: [
+          { username: username },
+          { countries: { $elemMatch: { country_code: country_code } } }
+        ]
+      };
+      const queryUser = { username: username };
+
+      //below are operators
+      const editCountry = { $set: { 'countries.$.notes': have_notes } };
+
+      const createCountry = {
+        $push: {
+          countries: { country_code: country_code, name: name, notes: notes }
+        }
+      };
+
+      const options = { new: true };
+
+      //logic for below:
+      //1. searches for document that includes user & country (findOne)
+      //2. If fails, that means user does not have the country
+      // it will create a new country object for that use with all of the information from the req.body (updateOne)
+      //3. If #1 succeeds, it will update the user countries code with the status_code from the req.body (findOneAndUpdate)
+      const found_user_country = await User.findOne(queryUserCountry);
+
+      if (found_user_country) {
+        // update the country in user
+        const updated_country = await User.findOneAndUpdate(
+          queryUserCountry,
+          editCountry,
+          options
+        );
+        if (updated_country) {
+          res.status(200).json(updated_country);
+        } else {
+          res.status(400).json({ error: 'failure to update country notes' });
         }
       } else {
         // create new country by updating the user
