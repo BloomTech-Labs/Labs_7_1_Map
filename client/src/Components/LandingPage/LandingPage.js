37bbecd4a1cfbbb87d30fa2815e75bae2796261b
odax <nr10@albion.edu>
2018-11-08 15:02:41 -0500

    started styling landing page, reset styles, added flexbox and outline

--- client/src/Components/LandingPage/LandingPage.js
+++ client/src/Components/LandingPage/LandingPage.js
@@ -19,24 +19,43 @@ const LandingPage = props => {
   return (
     <div className="LandingPage">
       {/* <img src={Logo} alt="logo" className="LandingPage__Logo" /> */}
-      <h1 className="LandingPage__Header">MapScratcher</h1>
-      <h3 className="LandingPage__Tagline">
-        Scratch the Itch to Track Your Trips
-      </h3>
-      <AppContextConsumer>
-        {value => {
-          return (
-            <LogInBay
-              failedSignUp={value.AppState.failedSignUp}
-              handleSignUp={value.handleSignUp}
-              failedSignUpMessage={value.AppState.failedSignUpMessage}
-              resetAppStateError={value.resetAppStateError}
-            />
-          );
-        }}
-      </AppContextConsumer>
-      {PopUp}
-      <Footer />
+      <div className="LandingPage__Container">
+        <div className="Container__Header">
+          <h1 className="Header__Title">MapScratcher</h1>
+        </div>
+        <div className="Container__Content">
+          <div className="Content__Left">
+            <h3 className="Left__Tagline">
+              <div className="Tagline__Phrase">
+                <p>Track where youve been.</p>
+                <p>Plan where youll go.</p>
+              </div>
+            </h3>
+            <div className="Left__LoginBayContainer">
+              <AppContextConsumer>
+                {value => {
+                  return (
+                    <LogInBay
+                      className="LoginBayContainer__LoginBay"
+                      failedSignUp={value.AppState.failedSignUp}
+                      handleSignUp={value.handleSignUp}
+                      failedSignUpMessage={value.AppState.failedSignUpMessage}
+                      resetAppStateError={value.resetAppStateError}
+                    />
+                  );
+                }}
+              </AppContextConsumer>
+              {PopUp}
+            </div>
+          </div>
+          <div className="Content__Right">
+            <div className="Right__Animation"/>
+          </div>
+        </div>
+        <div className="Container__Footer">
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 };
