04557c54d6333324a59bc04ee9af2991ab2c9c90
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-11-08 10:32:41 -0800

    Add colorPalette for country status
    Legend and map layer colors are setup to pull from the colorPalette
    object.

--- client/src/Components/Legend/Legend.js
+++ client/src/Components/Legend/Legend.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import './Legend.css';
+import { colorPalette } from '../Map/countryStyles.js';
 
 const Legend = () => {
   return (
     <div className="Legend">
-      <div className="Legend__NoInterest">
-        <span className="NoInterest__gray" />
-        No Interest
-      </div>
+      {/* <div className="Legend__NoInterest"> */}
+      {/*   <span className="NoInterest__gray" /> */}
+      {/*   No Interest */}
+      {/* </div> */}
       <div className="Legend__Wishlist">
-        <span className="Wishlist__purple" />
+        <span className="Wishlist__purple" style={{ "backgroundColor": colorPalette.wishlist }}/>
         Wish List
       </div>
       <div className="Legend__Transited">
-        <span className="Transited__yellow" />
+        <span className="Transited__yellow"  style={{ "backgroundColor": colorPalette.transited }}/>
         Transited
       </div>
       <div className="Legend__Visited">
-        <span className="Visited__red" />
+        <span className="Visited__red"  style={{ "backgroundColor": colorPalette.visited }}/>
         Visited
       </div>
       <div className="Legend__Lived">
-        <span className="Lived__blue" />
+        <span className="Lived__blue"  style={{ "backgroundColor": colorPalette.livedIn }}/>
         Lived
       </div>
     </div>
