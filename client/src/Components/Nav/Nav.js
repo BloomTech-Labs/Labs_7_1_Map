cfcc12fc8e74dc0408474b8e2736c2f5c65d6a9e
odax <nr10@albion.edu>
2018-11-15 02:53:59 -0500

    finished prop-types for landing page and children

--- client/src/Components/Nav/Nav.js
+++ client/src/Components/Nav/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AppContextConsumer } from '../../AppContext';
+import PropTypes from 'prop-types';
 import './Nav.css';
 import themeColors from '../themeColors.js';
 
