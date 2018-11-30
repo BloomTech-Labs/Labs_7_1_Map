4c9a58fa27e33376315be153e3b7c4ba1da59524
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-11-30 13:30:58 -0800

    Move Downshift component to SearchCountry

--- client/src/Components/SearchCountry/SearchCountry.js
+++ client/src/Components/SearchCountry/SearchCountry.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import Downshift from 'downshift';
 import PropTypes from 'prop-types';
 
+import geojson from '../Map/countries.geo.json';
 import themeColors from '../themeColors.js';
 import './SearchCountry.css';
+
+const countries = geojson.features;
+
+const onChange = country => {
+  alert(`you selected ${country.properties.name} - ${country.id}`);
+};
 
 class SearchCountry extends Component {
   state = {
@@ -16,21 +24,94 @@ class SearchCountry extends Component {
 
   render() {
     return (
-      <form className="SearchCountry" onSubmit={this.props.handleSearchSubmit}>
-        <input
-          className="SearchCountry__input"
-          type="text"
-          name="search"
-          value={this.formValue}
-          placeholder="Search Countries..."
-          onChange={e => this.handleSearchChange(e)}
-          style={{
-            backgroundColor: themeColors.background[this.props.theme],
-            color: themeColors.fontColor[this.props.theme],
-            border: `1px solid ${themeColors.borderColor[this.props.theme]}`
-          }}
-        />
-      </form>
+      <div className="SearchCountry">
+        <Downshift
+          onChange={onChange}
+          itemToString={country => (country ? country.properties.name : '')}
+        >
+          {({
+            getInputProps,
+            getItemProps,
+            isOpen,
+            inputValue,
+            highlightedIndex,
+            selectedItem,
+            highlightedItem,
+            getLabelProps
+          }) => (
+            <div>
+              {/* <label */}
+              {/*   style={{ marginTop: '1rem', display: 'block' }} */}
+              {/*   {...getLabelProps()} */}
+              {/* > */}
+              {/*   Search for a country */}
+              {/* </label>{' '} */}
+              {/* <br /> */}
+              <input
+                {...getInputProps({
+                  placeholder: 'Search countries'
+                })}
+                style={{
+                  backgroundColor: themeColors.background[this.props.theme],
+                  color: themeColors.fontColor[this.props.theme],
+                  border: `1px solid ${
+                    themeColors.borderColor[this.props.theme]
+                  }`
+                }}
+              />
+              {isOpen ? (
+                <div className="downshift-dropdown">
+                  {countries
+                    .filter(
+                      country =>
+                        !inputValue ||
+                        country.properties.name
+                          .toLowerCase()
+                          .includes(inputValue.toLowerCase())
+                    )
+                    .map((country, index) => (
+                      <div
+                        key={index}
+                        className="dropdown-item"
+                        {...getItemProps({
+                          key: country.properties.name,
+                          index,
+                          item: country
+                        })}
+                        style={{
+                          backgroundColor:
+                            highlightedIndex === index ? 'lightgray' : 'white',
+                          fontWeight:
+                            selectedItem === country ? 'bold' : 'normal'
+                        }}
+                      >
+                        {country.properties.name}
+                      </div>
+                    ))}
+                </div>
+              ) : null}
+            </div>
+          )}
+        </Downshift>
+        {/* <form */}
+        {/*   className="SearchCountry" */}
+        {/*   onSubmit={this.props.handleSearchSubmit} */}
+        {/* > */}
+        {/*   <input */}
+        {/*     className="SearchCountry__input" */}
+        {/*     type="text" */}
+        {/*     name="search" */}
+        {/*     value={this.formValue} */}
+        {/*     placeholder="Search Countries..." */}
+        {/*     onChange={e => this.handleSearchChange(e)} */}
+        {/*     style={{ */}
+        {/*       backgroundColor: themeColors.background[this.props.theme], */}
+        {/*       color: themeColors.fontColor[this.props.theme], */}
+        {/*       border: `1px solid ${themeColors.borderColor[this.props.theme]}` */}
+        {/*     }} */}
+        {/*   /> */}
+        {/* </form> */}
+      </div>
     );
   }
 }
